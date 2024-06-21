c16a1e93c1390f53c61796057c026532315b767e
Taste Haven <tastehaven@protonmail.com>
2024-06-21 23:05:20 +1200

    Refactor CSS and JS for improved structure and functionality, update color variables file, and update profile menu and sidebar behavior.

--- Resources/JavaScript/Top, side, bottom bar.js
+++ Resources/JavaScript/Top, side, bottom bar.js
@@ -1,27 +1,53 @@
+//* Top Bar
+// Profile pop up button
+document.addEventListener("DOMContentLoaded", (event) => {
+  const profileButton = document.getElementById("profileButton");
+  const profileMenu = document.getElementById("profileMenu");
+
+  // Hide the profile menu on page load
+  profileMenu.classList.add("hidden");
+
+  // Toggle the profile menu when the button is clicked
+  profileButton.addEventListener("click", (event) => {
+    profileMenu.classList.toggle("hidden");
+    event.stopPropagation(); // Prevent the click from propagating to the document
+  });
+
+  // Hide the profile menu when clicking outside of it
+  document.addEventListener("click", (event) => {
+    if (!profileMenu.classList.contains("hidden")) {
+      profileMenu.classList.add("hidden");
+    }
+  });
+
+  // Prevent the menu from hiding when clicking inside of it
+  profileMenu.addEventListener("click", (event) => {
+    event.stopPropagation();
+  });
+});
+
 //* Sidebar
 // Burger menu
 document.addEventListener("DOMContentLoaded", function () {
-  const burgerMenu = document.getElementById("burger-menu");
+  const burgerMenu = document.getElementById("sidebar-burger-button");
   const sidebar = document.getElementById("sidebar");
 
   burgerMenu.addEventListener("click", function () {
     if (sidebar.style.left === "0px") {
-      sidebar.style.left = "-250px"; // or "-100%" for mobile
+      sidebar.style.left = "-300px";
+      burgerMenu.classList.remove("burger-hidden");
     } else {
       sidebar.style.left = "0";
+      burgerMenu.classList.add("burger-hidden");
     }
   });
-});
 
-// Sidebar active state
-document.addEventListener("DOMContentLoaded", function () {
-  const navItems = document.querySelectorAll(".nav-item a");
-
-  navItems.forEach((item) => {
-    item.addEventListener("click", function () {
-      navItems.forEach((nav) => nav.classList.remove("active"));
-      this.classList.add("active");
-    });
+  // Close the sidebar when clicking outside of it
+  document.addEventListener("click", function (event) {
+    if (!sidebar.contains(event.target) && !burgerMenu.contains(event.target)) {
+      sidebar.style.left = "-250px";
+      burgerMenu.classList.remove("burger-hidden");
+    }
   });
 });
 
